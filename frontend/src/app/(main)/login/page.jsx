134f309876ce685a0845a255298b0ddd6bd5433a
sachin2745 <sachin.official648@gmail.com>
2024-03-26 11:21:32 +0530

    update

--- frontend/src/app/(main)/login/page.jsx
+++ frontend/src/app/(main)/login/page.jsx
@@ -20,6 +20,7 @@ import classes from './login.module.css';
 import cx from 'clsx';
 import { MantineProvider, Container, createTheme } from '@mantine/core';
 import Link from 'next/link';
+import { BackgroundImage, Center, Box } from '@mantine/core';
 
 const theme = createTheme({
   components: {
@@ -50,87 +51,80 @@ export function Login(props) {
     }
   })
 
-  
+
 
   return (
     <MantineProvider theme={theme} >
-      <Container size="responsive" px={0} bg="white" mt={50}>
-        <Paper radius="md" p="xl" withBorder {...props} h={500} size="xxs" className={classes.Container}>
-          <Text size="lg" fw={500}>
-            Welcome to Mantine, {type} with
-          </Text>
+      <Box mx="auto" >
+        <BackgroundImage
+          src="https://t3.ftcdn.net/jpg/03/55/60/70/360_F_355607062_zYMS8jaz4SfoykpWz5oViRVKL32IabTP.jpg"
+          radius="md"
+          h={700}
+        >
+          <Center p="md">
+            <Container size="responsive"  mt={100} h={700} w={700} >
+              <Paper radius="md" p="xl" withBorder {...props}  className={classes.Paper}>
+                <Text size="lg" fw={500}>
+                  Welcome to Mantine, {type} with
+                </Text>
 
-          <Group grow mb="md" mt="md">
-            <GoogleButton radius="xl">Google</GoogleButton>
-            <TwitterButton radius="xl">Twitter</TwitterButton>
-          </Group>
+                <Group grow mb="md" mt="md">
+                  <GoogleButton radius="xl" variant="outline" color="rgba(0, 0, 0, 1)">Google</GoogleButton>
+                  <TwitterButton radius="xl" variant="outline" color="rgba(0, 0, 0, 1)">Twitter</TwitterButton>
+                </Group>
 
-          <Divider label="Or continue with email" labelPosition="center" my="lg" />
+                <Divider label="Or continue with email" labelPosition="center" my="lg" />
 
-          <form onSubmit={form.onSubmit(() => { })}>
-            <Stack>
-              {type === "register" && (
-                <TextInput
-                  label="Name"
-                  placeholder="Your name"
-                  value={form.values.name}
-                  onChange={event =>
-                    form.setFieldValue("name", event.currentTarget.value)
-                  }
-                  radius="md"
-                />
-              )}
+                <form onSubmit={form.onSubmit(() => { })}>
+                  <Stack>
+                   
 
-              <TextInput
-                required
-                label="Email"
-                placeholder="hello@mantine.dev"
-                value={form.values.email}
-                onChange={event =>
-                  form.setFieldValue("email", event.currentTarget.value)
-                }
-                error={form.errors.email && "Invalid email"}
-                radius="md"
-              />
+                    <TextInput
+                      required
+                      label="Email"
+                      placeholder="hello@mantine.dev"
+                      value={form.values.email}
+                      onChange={event =>
+                        form.setFieldValue("email", event.currentTarget.value)
+                      }
+                      error={form.errors.email && "Invalid email"}
+                      radius="md"
+                      variant="filled"
+                    />
 
-              <PasswordInput
-                required
-                label="Password"
-                placeholder="Your password"
-                value={form.values.password}
-                onChange={event =>
-                  form.setFieldValue("password", event.currentTarget.value)
-                }
-                error={
-                  form.errors.password &&
-                  "Password should include at least 6 characters"
-                }
-                radius="md"
-              />
+                    <PasswordInput
+                      required
+                      label="Password"
+                      placeholder="Your password"
+                      value={form.values.password}
+                      onChange={event =>
+                        form.setFieldValue("password", event.currentTarget.value)
+                      }
+                      error={
+                        form.errors.password &&
+                        "Password should include at least 6 characters"
+                      }
+                      radius="md"
+                    />
 
-              {type === "register" && (
-                <Checkbox
-                  label="I accept terms and conditions"
-                  checked={form.values.terms}
-                  onChange={event =>
-                    form.setFieldValue("terms", event.currentTarget.checked)
-                  }
-                />
-              )}
-            </Stack>
+                    
+                  </Stack>
 
-            <Group justify="space-between" mt="xl">
-            <Anchor component={Link} underline="hover" type="button" c="dimmed" href="/signup" size="xs">
-                Don't have an account? Register
-              </Anchor>
-              <Button type="submit" radius="xl" variant="gradient"
-                  gradient={{ from: 'gray', to: 'red', deg: 11 }}>
-                {upperFirst(type)}
-              </Button>
-            </Group>
-          </form>
-        </Paper>
-      </Container>
+                  <Group justify="space-between" mt="xl">
+                    <Anchor component={Link} underline="hover" type="button" c="dimmed" href="/signup" size="xs">
+                      Don't have an account? Register
+                    </Anchor>
+                    <Button type="submit"  variant="outline" color="rgba(0, 0, 0, 1)"
+                    >
+                      {upperFirst(type)}
+                    </Button>
+                  </Group>
+                </form>
+              </Paper>
+            </Container>
+          </Center>
+        </BackgroundImage>
+      </Box>
     </MantineProvider>
   )
 }
