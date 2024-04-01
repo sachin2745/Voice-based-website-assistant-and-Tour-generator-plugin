379337e2982a6cc4f37ceea83c4fccdb82119e43
sachin2745 <sachin.official648@gmail.com>
2024-04-01 15:24:15 +0530

    hero section

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -9,90 +9,83 @@ import {
   Text,
   List,
   ThemeIcon,
-  rem
+  rem,
+  Overlay
 } from "@mantine/core"
 import { IconCheck } from "@tabler/icons-react"
 
 import classes from "./page.module.css"
 import FeaturesCards from './FeaturesCards'
 import Faqsection from './faqSection'
+import cx from 'clsx';
 
 
 const page = () => {
   return (
     <>
       <section>
-        <Container size="md" p={0}>
-          <div className={classes.inner}>
-            <div className={classes.content}>
-              <Title className={classes.title}>
-                <span className={classes.subtitle} >Welcome to<br /></span>
-                {/* <span className={classes.highlight}> */}
-                VoiceTour 
-                {/* </span>  */}
-                Navigator <br />{" "}
-                Plugin System
-              </Title>
-              <Text c="dimmed" mt="md">
-              Revolutionizing Website Navigation with Voice Technology
-              </Text>
+        <div className={classes.hero}>
+          <Overlay
+            gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
+            opacity={1}
+            zIndex={0}
+          />
+          <Container className={classes.container} size="md">
+            <Title className={classes.title}><span className={classes.highlight}>Welcome to</span> <br />VoiceTour Navigator <br />Plugin System</Title>
 
-              <List
-                mt={30}
-                spacing="sm"
-                size="sm"
-                icon={
-                  <ThemeIcon size={20} radius="xl">
-                    <IconCheck
-                      style={{ width: rem(12), height: rem(12) }}
-                      stroke={1.5}
-                    />
-                  </ThemeIcon>
-                }
-              >
-                {/* <List.Item>
+            <List className={classes.List} size="xl" mt="xl"
+
+              spacing="sm"
+              size="sm"
+              icon={
+                <ThemeIcon size={20} radius="xl">
+                  <IconCheck
+                    style={{ width: rem(12), height: rem(12) }}
+                    stroke={1.5}
+                  />
+                </ThemeIcon>
+              }
+            >
+              {/* <List.Item>
                   <b>Voice-Based Navigation</b> – build type safe applications, all
                   components and hooks export types
                 </List.Item> */}
-                <List.Item>
-                  <b>Voice-Based Navigation</b>
-                </List.Item>
-                <List.Item>
-                  <b>Personalized Website Tours</b>
-                </List.Item>
-                <List.Item>
-                  <b>Plugin Customization</b>
-                </List.Item>
-                <List.Item>
-                  <b>Intuitive User Experience</b>
-                </List.Item>
-                <List.Item>
-                  <b>Continuous Improvement</b>
-                </List.Item>
-              </List>
+              <List.Item>
+                <b>Voice-Based Navigation</b>
+              </List.Item>
+              <List.Item>
+                <b>Personalized Website Tours</b>
+              </List.Item>
+              <List.Item>
+                <b>Plugin Customization</b>
+              </List.Item>
+              <List.Item>
+                <b>Intuitive User Experience</b>
+              </List.Item>
+              <List.Item>
+                <b>Continuous Improvement</b>
+              </List.Item>
+            </List>
 
-              <Group mt={30}>
-                <Button radius="xl" size="md" className={classes.control}>
-                  Get started
-                </Button>
-                <Button
-                  variant="default"
-                  radius="xl"
-                  size="md"
-                  className={classes.control}
-                >
-                  Source code
-                </Button>
-              </Group>
-            </div>
-            <Image src="https://png.pngtree.com/background/20230503/original/pngtree-smart-robot-with-cute-face-colorful-plants-picture-image_2500123.jpg" className={classes.image} />
-          </div>
-        </Container>
+
+            
+            <div className={classes.controls}>
+          <Button className={classes.control} variant="outline" color="rgba(255, 255, 255, 1)" size="lg">
+            Get started
+          </Button>
+          <Button className={cx(classes.control, classes.secondaryControl)} variant="outline" color="rgba(255, 255, 255, 1)" size="lg">
+            Live demo
+          </Button>
+        </div>
+
+           
+          </Container>
+        </div>
       </section>
 
-        <FeaturesCards/>
+      <FeaturesCards />
 
-      <Faqsection/>
+      <Faqsection />
 
     </>
   )
