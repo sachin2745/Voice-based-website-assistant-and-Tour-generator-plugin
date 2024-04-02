3804a29ce83cbee63646c3d565692abf7d38644d
sachin2745 <sachin.official648@gmail.com>
2024-04-02 21:27:56 +0530

    image

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -10,74 +10,94 @@ import {
   List,
   ThemeIcon,
   rem,
-  Overlay
+  Overlay,
+  Box,
+  BackgroundImage
 } from "@mantine/core"
-import { IconCheck } from "@tabler/icons-react"
+import { IconCheck, IconProgressCheck } from "@tabler/icons-react"
 
 import classes from "./page.module.css"
 import FeaturesCards from './FeaturesCards'
 import Faqsection from './faqSection'
 import cx from 'clsx';
+import { GithubIcon } from '@mantinex/dev-icons'
 
 
 const page = () => {
   return (
     <>
-      <section >
-        <Container size="md">
-          <div className={classes.inner}>
-            <div className={classes.content}>
-            <Title className={classes.title}><span className={classes.highlight}>Welcome to</span> <br />VoiceTour Navigator <br />Plugin System</Title>
-              <Text c="dimmed" mt="md">
-              Revolutionizing Website Navigation with Voice Technology
-              </Text>
+      <section  >
+        <Box mx="auto" className={classes.box}>
 
-              <List className={classes.List} size="xl" mt="xl" spacing="sm"
-                size="sm"
-                icon={
-                  <ThemeIcon size={20} radius="xl">
-                    <IconCheck
-                      style={{ width: rem(12), height: rem(12) }}
-                      stroke={1.5}
-                    />
-                  </ThemeIcon>
-                }
-              >
-                {/* <List.Item>
+          <Container  >
+            <div className={classes.inner}>
+              <div className={classes.content}>
+                <Title className={classes.title}><span className={classes.highlight}>Welcome to</span> <br /><span className={classes.subtitle}>VoiceTour Navigator </span><br />Plugin System</Title>
+                <Text c="dimmed" mt="md">
+                  Revolutionizing Website Navigation with Voice Technology
+                </Text>
+
+                <List className={classes.List} size="xl" mt="xl" spacing="sm"
+                  size="sm"
+                  icon={
+                    <ThemeIcon size={20} radius="xl" color='#ffcc00'>
+                      <IconCheck
+                        style={{ width: rem(12), height: rem(12) }}
+                        stroke={5}
+
+                      />
+                    </ThemeIcon>
+                  }
+                >
+                  {/* <List.Item>
                   <b>Voice-Based Navigation</b> – build type safe applications, all
                   components and hooks export types
                 </List.Item> */}
-                <List.Item>
-                  <b>Voice-Based Navigation</b>
-                </List.Item>
-                <List.Item>
-                  <b>Personalized Website Tours</b>
-                </List.Item>
-                <List.Item>
-                  <b>Plugin Customization</b>
-                </List.Item>
-                <List.Item>
-                  <b>Intuitive User Experience</b>
-                </List.Item>
-                <List.Item>
-                  <b>Continuous Improvement</b>
-                </List.Item>
-              </List>
+                  <List.Item>
+                    <b>Voice-Based Navigation</b>
+                  </List.Item>
+                  <List.Item>
+                    <b>Personalized Website Tours</b>
+                  </List.Item>
+                  <List.Item>
+                    <b>Plugin Customization</b>
+                  </List.Item>
+                  <List.Item>
+                    <b>Intuitive User Experience</b>
+                  </List.Item>
+                  <List.Item>
+                    <b>Continuous Improvement</b>
+                  </List.Item>
+                </List>
 
-              <Group mt={15}>
-              <div className={classes.controls}>
-                <Button className={classes.control} variant="outline" color="rgba(255, 255, 255, 1)" size="lg">
-                  Get started
-                </Button>
-                <Button className={cx(classes.control, classes.secondaryControl)} variant="outline" color="rgba(255, 255, 255, 1)" size="lg">
-                  Live demo
-                </Button>
+
+                <Group className={classes.controls}>
+                  <Button
+                    size="sm"
+                    className={classes.control}
+                    variant="filled" color="#FFCC00"
+                  >
+                    Get started
+                  </Button>
+
+                  <Button
+                    component="a"
+                    href="https://github.com/mantinedev/mantine"
+                    size="sm"
+                    variant="default"
+                    className={classes.control}
+                    leftSection={<GithubIcon size={20} />}
+                  >
+                    GitHub
+                  </Button>
+                </Group>
+
               </div>
-              </Group>
+              <Image src="micimg.png" className={classes.image} />
             </div>
-            <Image src="loginimg.svg" className={classes.image} />
-          </div>
-        </Container>
+          </Container>
+
+        </Box>
       </section>
 
       <FeaturesCards />
