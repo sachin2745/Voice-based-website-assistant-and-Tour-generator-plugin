f0f4ea0921c7251c047ad58ccbc506907527cf31
sachin2745 <sachin.official648@gmail.com>
2024-04-15 13:33:11 +0530

    search bar setup

--- frontend/src/app/(main)/navbar.jsx
+++ frontend/src/app/(main)/navbar.jsx
@@ -178,7 +178,7 @@ function Navbar() {
                 Contact us
               </a>
             </Group>
-            <TextInput
+            {/* <TextInput
               placeholder="Search"
               size="xs"
               leftSection={<IconSearch style={{ width: rem(12), height: rem(12) }} stroke={1.5} />}
@@ -187,11 +187,20 @@ function Navbar() {
               styles={{ section: { pointerEvents: 'none' } }}
               mb="sm"
               mt='md'
-            />
+            /> */}
+            <div className={classes.group}>
+              <svg className={classes.icon} aria-hidden="true" viewBox="0 0 24 24">
+                <g>
+                  <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z" />
+                </g>
+              </svg>
+              <input placeholder="Search" type="search" className={classes.input} />
+            </div>
+
             {/* <Button component={Link} variant="default" href='/login'>Log in</Button> */}
-            <Button component={Link} variant='filled'  color='#4cbbff' href='/signup'>
-              <Lottie animationData={avatar} className={classes.avi}/>
-               SignUp </Button>
+            <Button component={Link} variant='filled' color='#4cbbff' href='/signup'>
+              <Lottie animationData={avatar} className={classes.avi} />
+              SignUp </Button>
 
             <ActionIcon
               onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
