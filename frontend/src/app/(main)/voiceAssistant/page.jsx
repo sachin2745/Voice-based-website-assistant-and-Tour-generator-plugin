799ac46b32e353cbb84333d6b0d6b1c13e80e5d0
sachin2745 <sachin.official648@gmail.com>
2024-04-21 10:47:10 +0530

    loader design

--- frontend/src/app/(main)/voiceAssistant/page.jsx
+++ frontend/src/app/(main)/voiceAssistant/page.jsx
@@ -1,32 +1,41 @@
 'use client'
-import { Title } from '@mantine/core'
+import { Container, Title } from '@mantine/core'
 import React from 'react'
-import { Audio, ThreeDots, Vortex } from 'react-loader-spinner'
+import { Audio, Bars, ThreeDots, Vortex } from 'react-loader-spinner'
 import classes from './voiceAssistant.module.css'
 
 const page = () => {
   return (
-    <div ><Title order={1} className={classes.Title}>Voice Assistant
-    </Title><Audio
-        height="80"
-        width="80"
-        radius="9"
-        color="green"
-        ariaLabel="three-dots-loading"
-        wrapperStyle
-        wrapperClass
-      />
-      <ThreeDots
-        visible={true}
-        height="80"
-        width="80"
-        color="white"
-        radius="9"
-        ariaLabel="three-dots-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-      />
+    <div >
+      <Container className={classes.Container}>
+        <Title order={1} className={classes.Title}>Voice Assistant
+        </Title><Audio
+          height="80"
+          radius="9"
+          color="white"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{ display: 'flex', justifyContent: 'center' }}
+          wrapperClass
 
+        />
+        <ThreeDots
+          visible={true}
+          color="white"
+          radius="9"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{ display: 'flex', justifyContent: 'center' }}
+          wrapperClass=""
+        />
+        <Bars
+          height="80"
+          width="80"
+          color="white"
+          ariaLabel="bars-loading"
+          wrapperStyle={{ display: 'flex', justifyContent: 'center' }}
+          wrapperClass=""
+          visible={true}
+        />
+      </Container>
     </div>
 
   )
